8f51d556539521cc44be92f6ba2db78de2f2ed3c
95suskjg <95suskjg@naver.com>
2022-11-17 22:01:56 +0900

    리액트 튜토리얼 22.11.17

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,6 +5,8 @@ import {Routes, Route, Link} from "react-router-dom";
 import Home from "./memo/Home";
 import About from "./memo/About";
 import Counter from "./memo/Counter";
+import Input from "./memo/Input";
+import Input2 from "./memo/Input2";
 
 function App() {
   return (
@@ -13,11 +15,15 @@ function App() {
             <Link to="/">Home</Link>
             <Link to="/about">about</Link>
             <Link to="/Counter">Counter</Link>
+            <Link to="/Input">Input</Link>
+            <Link to="/Input2">Input2</Link>
         </nav>
         <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="/about" element={<About />}/>
             <Route path="/Counter" element={<Counter />}/>
+            <Route path="/Input" element={<Input />}/>
+            <Route path="/Input2" element={<Input2 />}/>
         </Routes>
     </div>
   );
