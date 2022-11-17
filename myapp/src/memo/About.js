cc82b4574ef9fb17911bd0e920b63b515a5a96ab
95suskjg <95suskjg@naver.com>
2022-11-17 16:13:31 +0900

    리액트 튜토리얼 22.11.17

---
+++ myapp/src/memo/About.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+    return <div><h1>난 어바웃이야</h1></div>;
+}
+
+export default About;
---
+++ myapp/src/memo/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+    return <div><h1>난 홈이야</h1></div>;
+}
+
+export default Home;
